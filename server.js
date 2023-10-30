01714a4bfd59e1514d2a94b58f2d6591183408c3
Dale Lanto <dalelanto.work@gmail.com>
2023-10-31 01:51:55 +0800

    install and setup nodemon

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ app.get(`/`, (req, res) => {
   res.send(`Hello NODE API`);
 });
 
+app.get(`/blog`, (req, res) => {
+  res.send(`Hello Blog Hi`);
+});
+
 app.listen(3000, () => {
   console.log(`Node API app is running on port http://localhost:3000`);
 });
