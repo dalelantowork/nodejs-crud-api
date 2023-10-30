5412e3da84731855540cccd7cb5267c4cf4f2b1c
Dale Lanto <dalelanto.work@gmail.com>
2023-10-31 02:24:53 +0800

    add mongoose and mongodb

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
 //routes
 
-app.get(`/`, (req, res) => {
-  res.send(`Hello NODE API`);
+app.get(`/`, (reques, response) => {
+  response.send(`Hello NODE API`);
 });
 
-app.get(`/blog`, (req, res) => {
-  res.send(`Hello Blog Hi`);
+app.get(`/blog`, (request, response) => {
+  response.send(`Hello Blog Hi`);
 });
 
-app.listen(3000, () => {
-  console.log(`Node API app is running on port http://localhost:3000`);
-});
+// mongoose.set('strictQuery', false);
+mongoose
+  .connect(
+    "mongodb+srv://dalelantowork:8XPE7M4Bj0krXlLk@nodejsapi.gepbfqw.mongodb.net/Node-API?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log(`connected to mongodb`);
+    app.listen(3000, () => {
+      console.log(`Node API app is running on port http://localhost:3000`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
