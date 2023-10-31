4964b08c160d4ab8babfed6f149395e567ad585a
Dale Lanto <dalelanto.work@gmail.com>
2023-10-31 12:39:40 +0800

    add models and routes with crud

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
+const { response } = require("express");
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const Product = require("./models/productModel");
+
+// middleware
+app.use(express.json()); // for json
+app.use(express.urlencoded({ extended: false })); // for x-www-form-urlencoded
 
 //routes
 
@@ -10,6 +16,66 @@ app.get(`/`, (reques, response) => {
 
 app.get(`/blog`, (request, response) => {
   response.send(`Hello Blog Hi`);
+});
+
+// get all
+app.get(`/products`, async (request, response) => {
+  try {
+    const products = await Product.find({}); // {} means get all
+    response.status(200).json(products);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+});
+
+// get by id
+app.get(`/products/:id`, async (request, response) => {
+  try {
+    const { id } = request.params;
+    const product = await Product.findById(id);
+    response.status(200).json(product);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+});
+
+// save product
+app.post(`/products`, async (request, response) => {
+  try {
+    const product = await Product.create(request.body);
+    response.status(200).json(product);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+});
+
+// update product
+app.put(`/products/:id`, async (request, response) => {
+  try {
+    const { id } = request.params;
+    const product = await Product.findByIdAndUpdate(id, request.body);
+    if (!product) {
+      return response.status(404).json({ message: `product not found` });
+    }
+    const updatedProduct = await Product.findById(id);
+    response.status(200).json(updatedProduct);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
+});
+
+// delete product
+app.delete(`/products/:id`, async (request, response) => {
+  try {
+    const { id } = request.params;
+    const product = await Product.findByIdAndDelete(id, request.body);
+    if (!product) {
+      return response.status(404).json({ message: `product not found` });
+    }
+    response.status(200).json(product);
+  } catch (error) {
+    response.status(500).json({ message: error.message });
+  }
 });
 
 // mongoose.set('strictQuery', false);
